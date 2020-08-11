82e5e3cf8466acf54b8c754c0085e76d819a5e09
Naiema Gardner <naiema@Naiemas-MacBook-Pro.local>
2020-08-11 08:23:58 -0400

    Modified routes
    Commented out all routes except POST.
    Successfully posted new entry.

--- app/routes/entry_routes.js
+++ app/routes/entry_routes.js
@@ -8,17 +8,17 @@ const Entry = require('../models/entry')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
-const customErrors = require('../../lib/custom_errors')
+// const customErrors = require('../../lib/custom_errors')
 
 // we'll use this function to send 404 when non-existant document is requested
-const handle404 = customErrors.handle404
+// const handle404 = customErrors.handle404
 // we'll use this function to send 401 when a user tries to modify a resource
 // that's owned by someone else
-const requireOwnership = customErrors.requireOwnership
+// const requireOwnership = customErrors.requireOwnership
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
 // { entry: { title: '', text: 'foo' } } -> { entry: { text: 'foo' } }
-const removeBlanks = require('../../lib/remove_blank_fields')
+// const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `req.user`
@@ -29,46 +29,46 @@ const router = express.Router()
 
 // INDEX
 // GET ALL /entries
-router.get('/entries', requireToken, (req, res, next) => {
-  Entry.find()
-    .then(entries => {
-      // `entries` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
-      return entries.map(entry => entry.toObject())
-    })
-    // respond with status 200 and JSON of the entries
-    .then(entries => res.status(200).json({ entries: entries }))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// router.get('/entries', requireToken, (req, res, next) => {
+//   Entry.find()
+//     .then(entries => {
+//       // `entries` will be an array of Mongoose documents
+//       // we want to convert each one to a POJO, so we use `.map` to
+//       // apply `.toObject` to each one
+//       return entries.map(entry => entry.toObject())
+//     })
+//     // respond with status 200 and JSON of the entries
+//     .then(entries => res.status(200).json({ entries: entries }))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 // GET MY /entries/:id
-router.get('/entries/:id', requireToken, (req, res, next) => {
-  Entry.find()
-    .then(entries => {
-      // `entries` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
-      return entries.map(entry => entry.toObject())
-    })
-    // respond with status 200 and JSON of the entries
-    .then(entries => res.status(200).json({ entries: entries }))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// router.get('/entries/:id', requireToken, (req, res, next) => {
+//   Entry.find()
+//     .then(entries => {
+//       // `entries` will be an array of Mongoose documents
+//       // we want to convert each one to a POJO, so we use `.map` to
+//       // apply `.toObject` to each one
+//       return entries.map(entry => entry.toObject())
+//     })
+//     // respond with status 200 and JSON of the entries
+//     .then(entries => res.status(200).json({ entries: entries }))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 // SHOW
 // GET /entries/5a7db6c74d55bc51bdf39793
-router.get('/entries/:id', requireToken, (req, res, next) => {
-  // req.params.id will be set based on the `:id` in the route
-  Entry.findById(req.params.id)
-    .then(handle404)
-    // if `findById` is succesful, respond with 200 and "entry" JSON
-    .then(entry => res.status(200).json({ entry: entry.toObject() }))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// router.get('/entries/:id', requireToken, (req, res, next) => {
+//   // req.params.id will be set based on the `:id` in the route
+//   Entry.findById(req.params.id)
+//     .then(handle404)
+//     // if `findById` is succesful, respond with 200 and "entry" JSON
+//     .then(entry => res.status(200).json({ entry: entry.toObject() }))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 // CREATE
 // POST /entries
@@ -89,42 +89,42 @@ router.post('/entries', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /entries/5a7db6c74d55bc51bdf39793
-router.patch('/entries/:id', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
-  delete req.body.entry.owner
-
-  Entry.findById(req.params.id)
-    .then(handle404)
-    .then(entry => {
-      // pass the `req` object and the Mongoose record to `requireOwnership`
-      // it will throw an error if the current user isn't the owner
-      requireOwnership(req, entry)
-
-      // pass the result of Mongoose's `.update` to the next `.then`
-      return entry.updateOne(req.body.entry)
-    })
-    // if that succeeded, return 204 and no JSON
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// router.patch('/entries/:id', requireToken, removeBlanks, (req, res, next) => {
+//   // if the client attempts to change the `owner` property by including a new
+//   // owner, prevent that by deleting that key/value pair
+//   delete req.body.entry.owner
+//
+//   Entry.findById(req.params.id)
+//     .then(handle404)
+//     .then(entry => {
+//       // pass the `req` object and the Mongoose record to `requireOwnership`
+//       // it will throw an error if the current user isn't the owner
+//       requireOwnership(req, entry)
+//
+//       // pass the result of Mongoose's `.update` to the next `.then`
+//       return entry.updateOne(req.body.entry)
+//     })
+//     // if that succeeded, return 204 and no JSON
+//     .then(() => res.sendStatus(204))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 // DESTROY
 // DELETE /entries/5a7db6c74d55bc51bdf39793
-router.delete('/entries/:id', requireToken, (req, res, next) => {
-  Entry.findById(req.params.id)
-    .then(handle404)
-    .then(entry => {
-      // throw an error if current user doesn't own `entry`
-      requireOwnership(req, entry)
-      // delete the entry ONLY IF the above didn't throw
-      entry.deleteOne()
-    })
-    // send back 204 and no content if the deletion succeeded
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// router.delete('/entries/:id', requireToken, (req, res, next) => {
+//   Entry.findById(req.params.id)
+//     .then(handle404)
+//     .then(entry => {
+//       // throw an error if current user doesn't own `entry`
+//       requireOwnership(req, entry)
+//       // delete the entry ONLY IF the above didn't throw
+//       entry.deleteOne()
+//     })
+//     // send back 204 and no content if the deletion succeeded
+//     .then(() => res.sendStatus(204))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 module.exports = router
