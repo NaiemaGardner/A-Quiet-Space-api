1b0ebbe8127df3d2a4405883561f94bc72bd05c4
Naiema Gardner <naiema@Naiemas-MacBook-Pro.local>
2020-08-12 17:56:47 -0400

    INDEX method Commit
    GET all user entries retrieves same object as GET my user entries.
    Need to specify signed-in user entries.
    Possibly token variable.

--- app/routes/entry_routes.js
+++ app/routes/entry_routes.js
@@ -29,19 +29,19 @@ const router = express.Router()
 
 // INDEX
 // GET ALL /entries
-// router.get('/entries', requireToken, (req, res, next) => {
-//   Entry.find()
-//     .then(entries => {
-//       // `entries` will be an array of Mongoose documents
-//       // we want to convert each one to a POJO, so we use `.map` to
-//       // apply `.toObject` to each one
-//       return entries.map(entry => entry.toObject())
-//     })
-//     // respond with status 200 and JSON of the entries
-//     .then(entries => res.status(200).json({ entries: entries }))
-//     // if an error occurs, pass it to the handler
-//     .catch(next)
-// })
+router.get('/entries', requireToken, (req, res, next) => {
+  Entry.find()
+    .then(entries => {
+      // `entries` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return entries.map(entry => entry.toObject())
+    })
+    // respond with status 200 and JSON of the entries
+    .then(entries => res.status(200).json({ entries: entries }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
 
 // GET MY /entries/:id
 router.get('/entries/:id', requireToken, (req, res, next) => {
